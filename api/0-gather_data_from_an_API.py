d235972c41dee3fa18a67aacd1f2b6a46ef07f4b
fgmcolas <https://github.com/fgmcolas>
2024-04-15 04:19:39 +0200

    changing indentation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,30 +5,30 @@ import requests
 import sys
 
 
-url = "https://jsonplaceholder.typicode.com"
-employeeId = sys.argv[1]
-
-response = requests.get(
-    f"{url}/users/{employeeId}/todos",
-    params={"_expand": "user"}
-)
-data = response.json()
-
-if not len(data):
-    print("RequestError:", 404)
-    sys.exit(1)
-
-completed_tasks = [task for task in data if task["completed"]]
-total_tasks = len(data)
-total_done_tasks = len(completed_tasks)
-employee_name = data[0]["user"]["name"]
-
-print(f"Employee {employee_name} is done with tasks"
-      f"({total_done_tasks}/{total_tasks}):")
-for task in completed_tasks:
-    print(f"\t {task["title"]}")
-
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print(f"Usage: python3 {__file__} employee_id(int)")
         sys.exit(1)
+
+    url = "https://jsonplaceholder.typicode.com"
+    employeeId = sys.argv[1]
+
+    response = requests.get(
+        f"{url}/users/{employeeId}/todos",
+        params={"_expand": "user"}
+    )
+    data = response.json()
+
+    if not len(data):
+        print("RequestError:", 404)
+        sys.exit(1)
+
+    completed_tasks = [task for task in data if task["completed"]]
+    total_tasks = len(data)
+    total_done_tasks = len(completed_tasks)
+    employee_name = data[0]["user"]["name"]
+
+    print(f"Employee {employee_name} is done with tasks"
+          f"({total_done_tasks}/{total_tasks}):")
+    for task in completed_tasks:
+        print(f"\t {task["title"]}")
